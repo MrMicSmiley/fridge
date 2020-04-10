3ef7d065721d561a9bacbdc84e372b91ccf22ccc
MrMicSmiley <61665252+MrMicSmiley@users.noreply.github.com>
2020-04-10 17:20:10 -0500

    Combined Controller with Recipe

--- tagManagement.java
+++ tagManagement.java
@@ -2,23 +2,23 @@ import java.util.ArrayList;
 public class tagManagement {
    ArrayList<String> tags = new ArrayList<String>();
    
-   private boolean add_Tag(String tag){
+   public boolean add_Tag(String tag){
        return tags.add(tag);
    }
 
-   private boolean remove_Tag(String tag){
+   public boolean remove_Tag(String tag){
        return tags.remove(tag);
    }
 
-   private void changeTag(String oldTag, String newTag){
+   public void changeTag(String oldTag, String newTag){
        tags.set(tags.indexOf(oldTag), newTag);
    }
 
-   private boolean containsTag(String tagCompare){
+   public boolean containsTag(String tagCompare){
         return tags.contains(tagCompare);
    }
 
-   private boolean containsTags(ArrayList<String> tagsCompare){
+   public boolean containsTags(ArrayList<String> tagsCompare){
         //String temp;
         for (String temp: tagsCompare){
             if (tags.contains(temp) == false){
