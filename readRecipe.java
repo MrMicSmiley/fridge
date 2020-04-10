38ef0c39be65eef03aa8b6d0eec4ee2aaae50a13
WillA2 <48257971+WillA2@users.noreply.github.com>
2020-04-10 17:26:54 -0400

    Add files via upload

---
+++ readRecipe.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+public class readRecipe {
+    public void setUp(){
+
+        File file = new File("recipe.txt");
+        try{
+            String read;
+            Scanner sc = new Scanner(file); 
+            while(sc.hasNextLine()){
+                Recipe recipe = new Recipe (sc.nextLine(), sc.nextLine());
+                while((read = sc.nextLine()) != "***"){
+                    recipe.add_ingredients(read);
+                }
+                read = " ";
+                while((read = sc.nextLine()) != "***"){
+                    recipe.add_tag(read);
+                }
+                sc.nextLine();
+            }
+        }
+          catch(FileNotFoundException e){
+            printErrorMsg();
+          }
+    }
+
+    private void printErrorMsg(){
+        System.out.println("Cannot find file");
+    }
+}
