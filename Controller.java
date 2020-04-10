e01f192814c9a5346680d62f24b92e8cfa16adb2
Steven Nguyen <46589340+Stendonn@users.noreply.github.com>
2020-04-10 16:10:22 -0400

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 
 public class Controller {
-
+    ArrayList<String> ingredients = new ArrayList();
+    ArrayList<Recipe> allRecipes = new ArrayList();
+    ArrayList<Recipe> shownRecipe = new ArrayList();
 
 
 
 
     public static void main(String[] args){
-        ArrayList<String> ingredients = new ArrayList();
+
         //menu loop
 
 
@@ -21,7 +23,19 @@ public class Controller {
         ingredients.add(ingred);
 
         //add recipe
-        
+            //if user selected 4 and entered their option
+
+        String recipeName = "apple pie";
+        String recipeIngreds = "butter, flour, sugar, apple";
+        ArrayList<String> listIngredsforRecipe = new ArrayList();
+        Recipe temp = new Recipe("recipeName");
+        for(int i = 0; i < listIngredsforRecipe.size(); i++){
+            temp.addIngredient(listIngredsforRecipe.get(i));
+        }
+        allRecipes.add(temp);
+
+
+
         //quit
 
         //menu loop end
