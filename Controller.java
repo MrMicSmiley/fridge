4f0ea8880370a49cabe78cbede17332aac1963fb
Steven Nguyen <46589340+Stendonn@users.noreply.github.com>
2020-04-10 15:41:00 -0400

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Controller {
+
+
+
+
+
+    public static void main(String[] args){
+        ArrayList<String> ingredients = new ArrayList();
+        //menu loop
+
+
+
+        //browse recipes
+        //add to fridge
+        //add ingreddient
+
+            //if user selected 3 and entered their option
+        String ingred = "apple";
+        ingredients.add(ingred);
+
+        //add recipe
+        
+        //quit
+
+        //menu loop end
+
+        System.out.println(ingredients.toString());
+
+    }
+
+
+}
