4b1d6d3256369a479a7c85b54e9b1700e7aacba0
salhuss <53706728+salhuss@users.noreply.github.com>
2020-04-10 17:01:03 -0400

    Add files via upload

--- Recipe.java
+++ Recipe.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;;
 
-public class Recipe {
-	String rec_name;
-	ArrayList<String> ingredients;
+public class Recipe extends tagManagement {
+	private String rec_name;
+	private ArrayList<String> ingredients;
+	private String link;
 
-	public Recipe(String rec_name) {
-		this.rec_name = rec_name;
-		this.ingredients = new ArrayList<String>();
+	public Recipe() {
+		this.rec_name = null;
+		this.ingredients = null;
+		this.link = null;
 	}
 
-	boolean add_ingredient(String ingredient) {
+	public Recipe(String rec_name, String link) {
+		this.rec_name = rec_name;
+		this.ingredients = new ArrayList<String>();
+		this.link = link;
+	}
+
+	private boolean add_ingredient(String ingredient) {
 		if (ingredient.equals("") || ingredient.equals(" ")) {
 			return false;
 		}
@@ -20,19 +28,41 @@ public class Recipe {
 			}
 		}
 		ingredients.add(ingredient);
-		System.out.println("%s was added to the recipe.");
+		System.out.printf("%s was added to the recipe.", ingredient);
 		return true;
 	}
 
-	boolean rem_ingredient(String ingredient) {
-		if (ingredient.equals("") || ingredient.equals(" ")) {
-			return false;
-			for (String each : ingredients) {
-				if (each.equals(ingredient)) {
-					ingre
-					return true;
-				}
-			}	
-		return false;
+	private void getLink() {
+		System.out.println("Please follow link to view the recipe.");
+		System.out.printf("%s", this.link);
+	}
+
+	private void print_ingredients() {
+		if (ingredients == null) {
+			System.out.println("Nothing to see hear");
+			return;
+		}
+		int counter = 1;
+		for (String string : ingredients) {
+			System.out.println("Here is the list of all the ingredients in this recipe.");
+			System.out.printf("(%i) %s ", counter, string);
+			counter++;
 		}
 	}
+
+	private boolean rem_ingredient(String ingredient) {
+		if (ingredient.equals("") || ingredient.equals(" ")) {
+			System.out.println("Enter a valid ingredient to remove from the recipe.");
+			return false;
+		}
+		for (String each : ingredients) {
+			if (each.equals(ingredient)) {
+				ingredients.remove(ingredient);
+				System.out.printf("The %s was removed from recipe.");
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
